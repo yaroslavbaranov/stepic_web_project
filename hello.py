64e47d2c8978352062ec7f68483ef8e5acc0ebe5
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-23 20:08:17 +0300

    added hello.py ver.2

--- hello.py
+++ hello.py
@@ -1,18 +1,7 @@
-
-from cgi import parse_qs
-
+#!/usr/bin/python
 def application(environ, start_response):
-
-  query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-  body = []
-  for key, values in query.items():
-    for item in values:
-      body.append(key + "=" + item + "\r\n")
-   
-  status = '200 OK'
-  headers = [
-   ('Content-Type', 'text/plain')
-  ]
-  
-  start_response(status, headers)
-  return body
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return data
