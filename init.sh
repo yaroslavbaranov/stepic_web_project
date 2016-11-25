e8aa77c8b681b73aecfaf73751d7f61f8d9dfa33
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 15:21:18 +0300

    none11

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
