98ba3c6bc94218a0c5be1f592ab8d42217d7b18f
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 13:31:38 +0300

    haha

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
-
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 ﻿﻿sudo /etc/init.d/mysql start﻿
