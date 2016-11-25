f53562bb0ccba4f6e00ee2a782fb09d28715415a
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 12:11:14 +0300

    ffff

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
+
 sudo rm /etc/nginx/sites-enabled/default
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
-﻿
+﻿﻿sudo /etc/init.d/mysql start﻿
