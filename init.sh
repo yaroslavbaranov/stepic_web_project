fcc82104342a0a5004d45ddb59ab879944c76398
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 15:22:49 +0300

    none12

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
