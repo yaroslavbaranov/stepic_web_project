1269c128282491ff747914d0a1a7dec43b545ba8
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 13:39:38 +0300

    11

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test
+
+sudo rm -r /etc/gunicorn.d/*
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
 ﻿﻿sudo /etc/init.d/mysql start﻿
