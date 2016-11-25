af5206a0d803efa84b93252aadbd161babfe46a1
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 16:28:21 +0300

    none19

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),)]
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),)]
+
+def app(environ, start_responce):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = '\r\n'.join(env['QUERY_STRING'].split('&'))
+    start_responce(status, headers)
+    return body
