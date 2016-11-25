8cac2dba4bc59286c78bfd7937451c7557938b90
yaroslavbaranov <baranov.yaroslav@pharminnotech.com>
2016-11-25 15:00:30 +0300

    none8

--- hello.py
+++ hello.py
@@ -1,27 +1,4 @@
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-#                   encoding="utf8")]
-
-from cgi import parse_qs
-
-
 def application(environ, start_response):
-    queryString = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-    body = ''
-    print(queryString)
-    for key, value in queryString.items():
-        for element in value:
-            body += key + '='
-            body += element + '\r\n'
-
-    print(body)
-
-    status = '200 OK'
-
-    response_headers = [
-        ('Content-Type', 'text/plain'),
-    ]
-    start_response(status, response_headers)
-
-    return [body]
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
--- init.sh
+++ init.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo rm -r /etc/gunicorn.d/*
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/qa.py   /etc/gunicorn.d/qa.py
 sudo /etc/init.d/gunicorn restart
